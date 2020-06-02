c9071e42f221409615bba6d8be93ec5ca984df22
seunghui <zlxl0519@naver.com>
2020-06-02 19:07:45 +0900

    Step08_Array MainClass06 내용 수정

--- Step08_Array/src/test/main/MainClass06.java
+++ Step08_Array/src/test/main/MainClass06.java
@@ -11,29 +11,31 @@ public class MainClass06 {
 		 * 	모두다 입력 받은후 for 문을 이용해서 배열에 저장된
 		 * 	모든 문자열을 콘솔창에 순서대로 출력하는 코드를 작성해 보세요.
 		 */
-		String[] scan=new String[5];
 		
+		String[] str=new String[5];
 		
-		for(int i=0; i<scan.length;i++) {
-			scan[i]=new String();
-			Scanner scan2=new Scanner(System.in);
-			System.out.println("문자열을 입력해주세요 : ");
-			String str=scan2.next();
-			System.out.println(str);
+		System.out.println("문자를 입력해주세요 : ");
+		for(int i=0; i<str.length; i++) {
+			Scanner scan=new Scanner(System.in);
+			String str2=scan.next();
+			str[i]=str2;
+			System.out.println(str[i]);
 		}
+			
+		
 		/*
 		 * 	2. Random 객체를 이용해서 로또 번호를 6개 랜덤하게 얻어내서
 		 * 		배열에 저장한다.
 		 * 		모두다 저장이 되면 for 문을 이용해서 배열에 저장된 모든 로또 번호를 
 		 * 		콘솔창에 순서대로 출력하는 코드를 작성해 보세요.
 		 */
-		int[] in=new int[6];
 		
-		for(int i=0; i<in.length; i++) {
-			
+		int[] num=new int[6];
+		for(int i=0; i<num.length; i++) {
 			Random ran=new Random();
 			int lottoNum=ran.nextInt(45)+1;
-			System.out.println(lottoNum);
+			num[i]=lottoNum;
+			System.out.println(num[i]);
 		}
 	}
 }
