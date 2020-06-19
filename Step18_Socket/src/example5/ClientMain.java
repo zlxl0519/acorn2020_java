4617c8582cdd665b27467ddf6225e4b720163169
seunghui <zlxl0519@naver.com>
2020-06-19 18:12:01 +0900

    Socket 프로젝트 내용 추가함

--- Step18_Socket/src/example5/ClientMain.java
+++ Step18_Socket/src/example5/ClientMain.java
@@ -2,6 +2,7 @@ package example5;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -14,27 +15,57 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 /*
+ * 	JSON
+ * 	
+ * 	- Java Script Object Notation (자바스크립트 객체 표기법을 따르는 문자열)
+ * 
+ * 	- 데이터의 type 
+ * 	1. {	}	(Object)
+ * 	2. [	]	(Array)
+ * 	3.	"xxx"	(문자형)	
+ * 	4. 	10	or	10.1	(숫자형)
+ * 	5.	true or false	(boolean)
+ * 	6. null	(비워놓을때 )
+ * 
+ * 	- JSON 예제
+ * 	
+ * //{ "key" :value } - value 값은 6가지 타입으로 가질수 있다.
+ * 	{"num":1, "name":"김구라", "isMan":true, "phone":null} 
+ * 
+ * 	[10, 20, 30, 40, 50]
+ * 
+ * 	["김구라","해골", "원숭이"]
+ * 
+ * 	[{}, {},{}]
+ * 
+ * 	{"name" : "kim", "friends":["김구라", "해골", "원숭이"]}
+ * 
  * 	메세지의 종류
  * 	
  * 	1. 일반 대화 메세지 	
- * 		{"name" : "김구라", "msg" : "안녕하세요"} - JSONObject neme 이라는 키값에 "김구라" 가 있는것.
+ * 		{"type":"msg", "name" : "김구라", "content" : "안녕하세요"} - JSONObject name 이라는 키값에 "김구라" 가 있는것.
  * 	2.	누군가 입장 했다는 메세지	
- * 		{"enter" : "김구라"}
+ * 		{"type":"enter", "name" : "김구라"}
  * 	3. 누군가 퇴장 했다는 메세지	
- * 		{"out " : "원숭이"}
+ * 		{"type":"out ", "name" : "원숭이"}
  * 	4. 참여자 목록 메세지
- * 		{"members":["김구라", "해골", "원숭이"]}   // [] -JSONArray
+ * 		{"type":"members", "list":["김구라", "해골", "원숭이"]}   // [] -JSONArray
  * 		
  * 
  */
@@ -47,44 +78,11 @@ public class ClientMain extends JFrame implements ActionListener, KeyListener{
 	JTextArea area;
 	//대화명
 	String chatName;
+	//참여자 목록 JList
+	JList<String> jList;
+	
 	//생성자
 	public ClientMain() {
-		//대화명을 입력 받아서 필드에 저장한다.
-		chatName=JOptionPane.showInputDialog(this, "대화명을 입력하세요");
-		
-		setTitle("대화명:"+chatName);
-		//서버에 소켓 접속을 한다.
-		try {
-			//접속이 성공되면 Socket 객체의 참조값이 반환된다.
-			//반환되는 객체의 참조값을 필드에 저장해 놓는다.
-			socket=new Socket("192.168.0.9", 5000);//필드에 저장된다.
-			//문자열을 서버에 전송(출력Output) 하기// 접속한 서버에 출력할객체
-			//서버에 문자열을 출력할
-			//BufferedWriter 객체의 참조값을 얻어내서 필드에 저장해 놓는다.
-			OutputStream os=socket.getOutputStream();//socket객체의 메소드로 받아온 객체
-			//new 해서 쓰고 버리고...이런식
-			OutputStreamWriter osw=new OutputStreamWriter(os);//객체를 일회용으로 사용중
-			bw=new BufferedWriter(osw);
-			//내가 입장한다고 서버에 메세지를 보낸다.
-			//"{"enter" : "김구라"}" {} - jsonobject , [] -jsonarray
-			//String msg="{\"enter\":\""+chatName+"\"}";//" 밖에 있는 " 인식하지 말라고 \ 사용
-			
-			JSONObject jsonObj=new JSONObject();
-			jsonObj.put("enter", chatName);// 서버에서 chatName 을 빼내기 위해서는 서버에서 new JSONObject(chatName);
-			String msg=jsonObj.toString();
-			//BufferedWriter 객체를 이용해서 보내기
-			bw.write(msg);
-			bw.newLine();
-			
-			
-			
-			//서버로 부터 메세지를 받을 스레드도 시작을 시킨다.
-			new ClientThread().start(); // 다른 스레드로 동작해서 동작이 잡혀있지 않는다.
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		
 		//레이아웃을 BorderLayout 으로 지정하기
 		setLayout(new BorderLayout());
 		
@@ -115,11 +113,68 @@ public class ClientMain extends JFrame implements ActionListener, KeyListener{
 				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		
+		
+		
 		//프레임의 가운데에 배치하기
 		add(scroll, BorderLayout.CENTER);
 		
 		//엔터키로 메세지 전송 가능하게 하기 위해
 		tf_msg.addKeyListener(this);
+		//String[] 에 JList 공간 확보를 위해 임시 문자열을 넣는다.
+		String[] title= {"참여자 목록"};
+		jList=new JList<String>(title);
+		jList.setBackground(Color.GREEN);
+		
+		JScrollPane scroll2=new JScrollPane(jList,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		//페널에 JList 를 배치하고
+		JPanel rightPanel=new JPanel();
+		rightPanel.add(scroll2);
+		
+		
+		//페널을 프레임의 동쪽에 배치
+		add(rightPanel, BorderLayout.EAST);
+		
+		
+		
+		
+		//대화명을 입력 받아서 필드에 저장한다.
+		chatName=JOptionPane.showInputDialog(this, "대화명을 입력하세요");
+		
+		setTitle("대화명:"+chatName);
+		//서버에 소켓 접속을 한다.
+		try {
+			//접속이 성공되면 Socket 객체의 참조값이 반환된다.
+			//반환되는 객체의 참조값을 필드에 저장해 놓는다.
+			socket=new Socket("192.168.0.30", 5000);//필드에 저장된다.
+			//문자열을 서버에 전송(출력Output) 하기// 접속한 서버에 출력할객체
+			//서버에 문자열을 출력할
+			//BufferedWriter 객체의 참조값을 얻어내서 필드에 저장해 놓는다.
+			OutputStream os=socket.getOutputStream();//socket객체의 메소드로 받아온 객체
+			//new 해서 쓰고 버리고...이런식
+			OutputStreamWriter osw=new OutputStreamWriter(os);//객체를 일회용으로 사용중
+			bw=new BufferedWriter(osw);
+			
+			//서버로 부터 메세지를 받을 스레드도 시작을 시킨다.
+			new ClientThread().start(); // 다른 스레드로 동작해서 동작이 잡혀있지 않는다.
+			
+			//내가 입장한다고 서버에 메세지를 보낸다.
+			//String msg="{\"enter\":\""+chatName+"\"}";//" 밖에 있는 " 인식하지 말라고 \ 사용
+			//"{"type":"enter", "name" : "대화명"}" 
+			JSONObject jsonObj=new JSONObject();
+			jsonObj.put("type", "enter");// key 값, value 값
+			jsonObj.put("name", chatName);
+			String msg=jsonObj.toString();
+			//BufferedWriter 객체를 이용해서 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
 	}//생성자 종료
 	
@@ -141,8 +196,18 @@ public class ClientMain extends JFrame implements ActionListener, KeyListener{
 		//전송할 문자열
 		String msg=tf_msg.getText();//메소드가 리턴해준 객체
 		try {
+			//JSONObject 객체를 생성해서 정보를 구성하고
+			//	1. 일반 대화 메세지 	
+			// {"type":"msg", "name" : "김구라", "content" : "안녕하세요"}
+			JSONObject jsonObj=new JSONObject();
+			jsonObj.put("type", "msg");
+			jsonObj.put("name", chatName);
+			jsonObj.put("content", msg);
+			//JSON 문자열을 얻어낸다.
+			String json=jsonObj.toString();
+			
 			//필드에 있는 BufferedWriter 객체의 참조값을 이용해서 서버에 문자열 출력하기
-			bw.write(chatName+" : "+msg);
+			bw.write(json);
 			bw.newLine();//개행기호도 출력 (서버에서 줄단위로 읽어낼 예정)
 			bw.flush();
 		}catch(Exception ie) {
@@ -165,8 +230,8 @@ public class ClientMain extends JFrame implements ActionListener, KeyListener{
 				while(true) {
 					//서버로부터 문자열이 전송되는지 대기한다.
 					String msg=br.readLine();
-					area.append(msg);
-					area.append("\r\n");// 개행 기호도 출력하기
+					//메소드를 호출하면서
+					updateTextArea(msg);
 					//최근 추가된 글 내용이 보일수 있도록
 					int docLength=area.getDocument().getLength();
 					area.setCaretPosition(docLength);
@@ -178,7 +243,51 @@ public class ClientMain extends JFrame implements ActionListener, KeyListener{
 				
 			}
 		}//run 종료
-	}
+		
+		//JTextArea 에 문자열을 출력하는 메소드
+		public void updateTextArea(String msg) {
+			try {
+				JSONObject jsonObj=new JSONObject(msg);
+				String type=jsonObj.getString("type");
+				if(type.equals("enter")) {//입장 메세지라면
+					//누가 입장했는지 읽어낸다.
+					String name=jsonObj.getString("name");
+					area.append("["+name+"] 님이 입장했습니다.");
+					area.append("\r\n");
+				}else if(type.equals("msg")) {//대화 메세지라면
+					//누가
+					String name=jsonObj.getString("name");
+					//어떤 내용을
+					String content=jsonObj.getString("content");
+					//출력하기
+					area.append(name+" : "+content);
+					area.append("\r\n");
+				}else if(type.equals("out")) {
+					//누가 
+					String name=jsonObj.getString("name");
+					//출력하기
+					area.append("[[ "+name+" ]] 님이 퇴장했습니다.");
+					area.append("\r\n");
+				}else if(type.equals("members")) {//대화 참여자 목록이 도착
+					//list 라는 키값으로 저장된 JSONArray 객체를 얻어온다.
+					JSONArray arr=jsonObj.getJSONArray("list");
+					//참여자 목록을 저장할 Vector
+					Vector<String> list=new Vector<>();
+					list.add("참여자 목록");
+					//반복문 돌면서 참여자 목록을 다시 넣어준다.
+					for(int i=0; i<arr.length(); i++) {
+						String tmp=arr.getString(i);
+						list.add(tmp);
+					}
+					//JList 에 참여자 목록 연결하기
+					jList.setListData(list);
+				}
+			}catch(JSONException je) {
+				je.printStackTrace();
+			}
+		}
+		
+	}// class ClientThread
 	@Override
 	public void keyPressed(KeyEvent e) {
 		//눌러진 키의 코드값
--- Step18_Socket/src/example5/ServerMain.java
+++ Step18_Socket/src/example5/ServerMain.java
@@ -2,6 +2,9 @@ package example5;
 
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -15,7 +18,7 @@ import java.util.ArrayList;
 
 public class ServerMain {//통신관련 동작만 있다.
 	//필드
-	static List<ServerThread> threadList=new ArrayList<>();
+	static List<ServerThread> threadList=new ArrayList<>();//하나하나씩 chatName 으로 쓴 내용이 들어있다.
 	public static void main(String[] args) {
 		// 필요한 객체를 저장할 지역변수 미리 만들기
 		ServerSocket serverSocket=null;
@@ -23,20 +26,16 @@ public class ServerMain {//통신관련 동작만 있다.
 		try {
 			// 5000 번 통신 port 을 열고 클라이언트의 접속을 기다린다.
 			serverSocket=new ServerSocket(5000);
-			/*
-			 * 	accept() 메소드는 클라이언트가 실제 접속을 할때 까지 리턴하지 않고 
-			 * 	블록킹 되는 메소드 이다.
-			 * 	클라이언트가 접속을 해오면 Socket 객체의 참조값을 반환하면서 리턴된다.
-			 */
 			while(true) {
 				System.out.println("클라이언트의 Socket 연결 요청을 대기합니다.");
+				//클라이언트의 소켓 접속을 기다린다.
 				socket=serverSocket.accept();// 접속한 클라이언트와 1대1로 연결되있는것.
 				System.out.println("클라이언트가 접속을 했습니다.");
 				//방금 접속한 클라이언트를 응대할 스레드를 시작시킨다.
 				ServerThread thread=new ServerThread(socket);
 				thread.start();
 				//생성하고 시작한 스레드의 참조값을 List에 저장하기
-				threadList.add(thread);
+				threadList.add(thread);// 서버스레드 참조값을 넣어놓은 리스트
 			}
 			
 		}catch(Exception e){
@@ -56,6 +55,8 @@ public class ServerMain {//통신관련 동작만 있다.
 		Socket socket;
 		//클라이언트에게 출력할수 있는 문자열이 있는 객체
 		BufferedWriter bw;
+		//클라이언트의 대화명을 저장할 필드
+		String chatName;
 		
 		//생성자의 인자로 Socket 객체를 전달받도록 한다.
 		public ServerThread(Socket socket) {
@@ -74,14 +75,39 @@ public class ServerMain {//통신관련 동작만 있다.
 			}
 		}
 		
+		//참여자 목록을 얻어내서 Client 에게 출력해주는 메소드
+		public void sendChatNameList() {
+			//4. 참여자 목록 메세지
+			// {"type":"members", "list":["김구라", "해골", "원숭이"]}
+			JSONObject jsonObj=new JSONObject();
+			JSONArray jsonArr=new JSONArray();
+			// 샘플 [ "김구라", "해골", "원숭이" ]
+			//jsonArr.put(0, "김구라");
+			//jsonArr.put(1, "해골");
+			//jsonArr.put(2, "원숭이");
+			
+			// 스레드 리스트에서 대화명을 순서대로 참조해서 JSONArray 객체에 순서대로 넣기
+			for(int i=0; i<threadList.size(); i++) {
+				ServerThread tmp=threadList.get(i);
+				jsonArr.put(i, tmp.chatName);
+			}
+			
+			jsonObj.put("type", "members");
+			jsonObj.put("list", jsonArr);
+			
+			try {
+				sendMessage(jsonObj.toString());//{"type" : "members","list" : []}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
 		// 새로운 스레드를 호출하려면 new ServerThread().start();
 		//새로운 작업 단위가 시작되는 run() 메소드
 		@Override
 		public void run() {
 			try {
-				//클라이언트 ip 주소
-				String clientIp=socket.getInetAddress().getHostAddress();
-				System.out.println("접속한 클라이언트의 아이피: "+clientIp);
 				//클라이언트로 부터 읽어들일 (input) 객체의 참조값 얻어오기
 				InputStream is=socket.getInputStream();
 				InputStreamReader isr=new InputStreamReader(is);
@@ -94,19 +120,20 @@ public class ServerMain {//통신관련 동작만 있다.
 				bw=new BufferedWriter(osw);
 				
 				while(true) {
-					/*
-					 * 	클라이언트가 문자열을 한줄 (개행기호와 함께) 보내면
-					 * 	readLine() 메소드가 리턴 하면서 보낸 문자열을 가지고 온다.
-					 * 	보내지 않으면 계속 블로킹 되어서 대기하고 있다가
-					 * 	접속이 끈기면 Exception 이 발생하거나 혹은 null 이 
-					 * 	리턴 된다.
-					 * 	따라서 null 이 리턴되면 반복문을 빠져 나가게 break 문을 만나도록
-					 * 	한다.
-					 * 	실행순서가 try 블럭을 벗어나면 run() 메소드가 리턴하게 되고
-					 * 	run() 메소드가 리턴되면 해당 스레드는 종료가 된다.
-					 */
+					//클라이언트가 전송하는 문자열을 읽어낸다.
 					String msg=br.readLine(); // 메세지를 전송할때까지 기다린다.
-					System.out.println("메세지:"+msg);
+					//전송된 JSON 문자열을 사용할 준비를 한다.
+					JSONObject jsonObj=new JSONObject(msg);
+					//type 을 읽어낸다.
+					String type=jsonObj.getString("type");
+					if(type.equals("enter")) {
+						//현재 스레드가 대응하는 클라이언트의 대화명을 필드에 저장한다.
+						String chatName=jsonObj.getString("name");
+						this.chatName=chatName;
+						//대화명 목록을 보내준다.
+						sendChatNameList();
+					}
+					
 					//클라이언트에게 동일한 메세지를 보내는 메소드를 호출한다.
 					sendMessage(msg);
 					if(msg==null) {// 클라이언트의 접속이 끈겼기 때문에
@@ -119,7 +146,14 @@ public class ServerMain {//통신관련 동작만 있다.
 			}finally {
 				//접속이 끈겨서 종료되는 스레드는 List에서 제거한다.
 				threadList.remove(this); // 인덱스로 삭제도 되고 참조값을 찾아서 remove 하는 것도 있다.
+				// this 가 퇴장 한다고 메세지를 보낸다.
 				try {
+					JSONObject jsonObj=new JSONObject();
+					jsonObj.put("type", "out");
+					jsonObj.put("name", chatName);
+					sendMessage(jsonObj.toString());
+					//대화명 목록을 보내준다.
+					sendChatNameList();
 					if(socket!=null)socket.close();
 				}catch(Exception e) {}
 			}//finally 종료
