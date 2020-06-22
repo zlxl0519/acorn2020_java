76e1802d5b6bb9654539d4280bfc4a80319a6f8c
seunghui <zlxl0519@naver.com>
2020-06-22 19:29:46 +0900

    MainClass10 내용 추가함

--- Step19_JDBC/src/test/main/MainClass10.java
+++ Step19_JDBC/src/test/main/MainClass10.java
@@ -12,17 +12,16 @@ public class MainClass10 {
 		MemberDao dao=MemberDao.getInstance();
 		//새로 추가할 회원의 정보라면
 		MemberDto dto=new MemberDto();
-		//dto.setName("덩어리");
-		//dto.setAddr("상도동");
-		////MemberDao 객체의 메소드를 활용해서 저장할수 있다.
-		//dao.insert(dto);
-		//
-		//MemberDao dao2=MemberDao.getInstance();// dao 에 들어있는 참조값과 같다. Connection 객체는 갯수가 많지 않다.
-		////수정할 회원의 정보라면
-		//MemberDto dto2= new MemberDto(1, "김구라", "독산동");
-		//dao2.update(dto2);
+		dto.setName("덩어리");
+		dto.setAddr("상도동");
+		//MemberDao 객체의 메소드를 활용해서 저장할수 있다.
+		dao.insert(dto);
 		
-		dao.getData(1);
+		MemberDao dao2=MemberDao.getInstance();// dao 에 들어있는 참조값과 같다. Connection 객체는 갯수가 많지 않다.
+		//수정할 회원의 정보라면
+		MemberDto dto2= new MemberDto(1, "김구라", "독산동");
+		dao2.update(dto2);
+		
 		
 	}
 }
